8b9d0662679a9d9554a59b296bc8e5510552cd35
jvanhamlin <jonathonvanhamlin@gmail.com>
2015-04-03 11:32:14 -0400

    Initial Commit

---
+++ SurfShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurfShop.Startup))]
+namespace SurfShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
