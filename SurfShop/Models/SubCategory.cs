98d2f8dcb91e4c35f5d63db2dea22d385a4c2b08
jvanhamlin <jonathonvanhamlin@gmail.com>
2015-04-04 13:52:40 -0400

    Initial Commit

---
+++ SurfShop/Models/SubCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurfShop.Models
+{
+	public class SubCategory
+	{
+		public int SubCategoryID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int CategoryID { get; set; }
+
+		public virtual ICollection<Product> Products { get; set; }
+	}
+}
