58cf1792674971bd1748db25d43418c85669a093
jvanhamlin <jonathonvanhamlin@gmail.com>
2015-04-07 17:37:29 -0400

    Added SurfShopContext since it was untracked

---
+++ SurfShop/Models/SurfShopContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SurfShop.Models
+{
+	public class SurfShopContext : DbContext
+	{
+		public SurfShopContext() : base("SurfShopContext")
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Company> Companies { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<SubCategory> SubCategories { get; set; }
+
+	}
+}
